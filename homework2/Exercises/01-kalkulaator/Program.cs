a0a21e4b62d6f4f1e9731c854abe244c85dc4ca5
siimsanderv <siimsander@outlook.com>
2017-11-03 15:08:50 +0200

    teine kodutöö uuendatud

--- homework2/Exercises/01-kalkulaator/Program.cs
+++ homework2/Exercises/01-kalkulaator/Program.cs
@@ -9,49 +9,52 @@ namespace _01_kalkulaator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("See on kalkulaator, millega saab liita, lahutada, korrutada ja jagada omavahel 2 arvu. Väljumiseks sisestage 'exit'");
-            Console.WriteLine("Komakoha jaoks kasutage '.'");
-            try
+            Console.WriteLine("See on kalkulaator, millega saab liita, lahutada, korrutada, jagada ja astendada omavahel 2 arvu. Väljumiseks sisestage 'exit'");
+            Console.WriteLine("Komakoha jaoks kasutage '.'\n");
+            do //Programm töötab loopina, ehk ei lähe kinni pärast esimest arvutust
             {
-                do
-                {
-                    Console.Write("\nSisestage esimene arv: ");
-                    string firstInp = Console.ReadLine();
-                    if (firstInp == "exit")
+                try //Kontrollib, kas programmi töötamise ajal (näiteks, kui kasutaja sisestab midagi) tekib mõni error.
+                { 
+                    Console.Write("Sisestage esimene arv: ");
+                    string firstInp = Console.ReadLine(); // Kasutaja sisestab arvu või "exit".
+                    if (firstInp == "exit") // Kontrollib, kas sisestus on "exit", kui jah, siis programm läheb kinni.
                         break;
-                    double firstNum = double.Parse(firstInp);
-                    Console.Write("Sisestage teine arv: ");
-                    string secondInp = Console.ReadLine();
-                    if (secondInp == "exit")
+                    double firstNum = double.Parse(firstInp); // Muudab sisestuse stringist ujukomaarvuks
+                    Console.Write("Sisestage teine arv: "); 
+                    string secondInp = Console.ReadLine(); // Kasutaja sisestab arvu või "exit".
+                    if (secondInp == "exit") // Kontrollib, kas sisestus on "exit", kui jah, siis programm läheb kinni.
                         break;
-                    double secondNum = double.Parse(secondInp);
-                    Console.Write("Kas soovite arve liita (+), lahutada (-), korrutada (*) või jagada (/): ");
-                    string thirdInp = Console.ReadLine();
-                    if (thirdInp == "exit")
+                    double secondNum = double.Parse(secondInp); // Muudab sisestuse stringist ujukomaarvuks.
+                    Console.Write("Kas soovite arve liita (+), lahutada (-), korrutada (*), jagada (/) või astendada (^): ");
+                    string thirdInp = Console.ReadLine(); // Kasutaja sisestab märgi või "exit".
+                    if (thirdInp == "exit")  // Kontrollib, kas sisestus on "exit", kui jah, siis programm läheb kinni.
                         break;
-                    char opSign = char.Parse(thirdInp);
-                    switch (opSign)
+                    char opSign = char.Parse(thirdInp); // Muudab sisestuse stringist chariks.
+                    switch (opSign) // Kontrollib, mis märgi kasutaja sisestas.
                     {
-                        case '+':
+                        case '+': // Kui kasutaja sisestas "+", siis programm liidab eelnevalt sisestatud arvud ja näitab tulemust.
                             Console.WriteLine($"{firstNum} + {secondNum} = {firstNum + secondNum}\n");
                             break;
-                        case '-':
+                        case '-': // Kui kasutaja sisestas "-", siis programm lahutab eelnevalt sisestatud arvud ja näitab tulemust.
                             Console.WriteLine($"{firstNum} - {secondNum} = {firstNum - secondNum}\n");
                             break;
-                        case '*':
+                        case '*': // Kui kasutaja sisestas "*", siis programm korrutab eelnevalt sisestatud arvud ja näitab tulemust.
                             Console.WriteLine($"{firstNum} * {secondNum} = {firstNum * secondNum}\n");
                             break;
-                        case '/':
+                        case '/': // Kui kasutaja sisestas "/", siis programm jagab eelnevalt sisestatud arvud ja näitab tulemust.
                             Console.WriteLine($"{firstNum} / {secondNum} = {firstNum / secondNum}\n");
+                            break;
+                        case '^': // Kui kasutaja sisestas "^", siis programm astendab eelnevalt sisestatud esimese arvu teise arvuga ja näitab tulemust.
+                            Console.WriteLine($"{firstNum} ^ {secondNum} = {Math.Pow(firstNum, secondNum)}\n");
                             break;
                     }
                 }
-                while (true);
+                catch(Exception) //Kui programm leiab errori, siis näitab kasutajale allolevat teksti.
+                {
+                    Console.WriteLine("Kasutage ainult arve ja väljumiseks 'exit'.\n");
+                }
             }
-            catch(Exception)
-            {
-                Console.WriteLine("Kasutage ainult arve ja väljumiseks 'exit'.");
-            }
+            while (true); //Programm töötab nii kaua, kuni kasutaja sisestab "exit".
         }   
     }
 }
