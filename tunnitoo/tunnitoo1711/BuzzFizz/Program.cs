88bb24eef7042a35e8058af772c88293de748563
siimsanderv <missionsgta4@gmail.com>
2017-11-17 13:45:42 +0200

    added buzzfizz

---
+++ tunnitoo/tunnitoo1711/BuzzFizz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuzzFizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=100;i++)
+            {
+                if(i%3==0 && i%5==0)
+                    Console.WriteLine("BuzzFizz");
+                else if (i%5==0)
+                    Console.WriteLine("Fizz");
+                else if (i%3==0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
