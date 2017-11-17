4694ea8b89e56e232e367c65a345429cc9fc9ba2
siimsanderv <missionsgta4@gmail.com>
2017-11-17 17:20:16 +0200

    added assignment

---
+++ tunnitoo/tunnitoo1711/tunnitoo17.11/1-PrintGreeting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PrintGreeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mitu nime");
+            int amt = int.Parse(Console.ReadLine());
+            string[] names = new string[amt];
+            for (int i = 0; i < amt; i++)
+            {
+                Console.Write("Sisesta nimi: ");
+               names[i] = Console.ReadLine();
+            }
+            foreach (var name in names)
+            {
+                PrintGreeting(name);
+            }
+            Console.ReadKey();
+        }
+        static void PrintGreeting(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
