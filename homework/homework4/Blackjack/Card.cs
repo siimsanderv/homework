98ad54c6d9578c5c8fd6b70174b4d630096274b0
siimsanderv <siimsander@outlook.com>
2018-01-14 20:55:26 +0200

    finished Blackjack

---
+++ homework/homework4/Blackjack/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blackjack
+{
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public Face Face { get; set; }
+        public int Value { get; set; }
+    }
+}
--- homework/homework4/Blackjack/Program.cs
+++ homework/homework4/Blackjack/Program.cs
@@ -7,222 +7,133 @@ namespace Blackjack
 {
     class Program
     {
+
+        // All aces are 11 points. In real life, one can choose whether it is 1 or 11
+        // When user draws a card, house always draws one as-well. In real life, house can decide based on some rules...  
+
+        static Deck deck;
+        static List<Card> userHand;
+        static List<Card> dealerHand;
         static void Main(string[] args)
-        { 
-            // All aces are 11 points. In real life, one can choose whether it is 1 or 11
-            // When user draws a card, house always draws one as-well. In real life, house can decide based on some rules...  
-
+        {
             Console.WriteLine("Welcome to the game of Blackjack!");
+            deck = new Deck(); //genereerib uue kaardipaki
+            deck.Shuffle(); //segab kaardipaki
+            DealHand(); //alustab mängu
+            Console.ReadKey();
+        }
+
+        static void DealHand()
+        {
             Console.WriteLine();
-            string[] Suit = new string[] { "S", "D", "H", "C" }; //list of card suits
-            Random rnd = new Random();
-            int[] playerRank = new int[10]; //empty list of player's cards, which gets value of 1-13 at line 30
-            int[] dealerRank = new int[10]; //empty list of dealer's cards, which gets value of 1-13 at line 31
-            string[] playerCardRank = new string[10]; //list that contains actual rank of player's cards (2-10,j,q,k,a)
-            string[] dealerCardRank = new string[10]; //list that contains actual rank of dealer's cards (2-10,j,q,k,a)
 
-            int index = 0;
-            int playerSum = 0; //sum of player's card values
-            int dealerSum = 0; //sum of dealer's card values
-            int playerCardVal = 0; //actual value of player's card (2-10; j,q,k=10;a=1 or 10)
-            int dealerCardVal = 0; //actualy value of dealer's card (2-10; j,q,k=10;a=1 or 10)
-            for (int i = 0; i < 2; i++)
-            {
-                playerRank[i] = rnd.Next(1, 14);
-                dealerRank[i] = rnd.Next(1, 14);
-                if (playerRank[i] == 1)
-                {
-                    playerCardRank[i] = "A";
-                    playerCardVal = 11;
-                }
-                else if (playerRank[i] < 10)
-                {
-                    playerCardVal = playerRank[i];
-                    playerCardRank[i] = $"{playerRank[i]}";
-                }
-                else if (playerRank[i] >= 10)
-                {
-                    playerCardVal = 10;
-                    switch(playerRank[i])
-                    {
-                        case 10:
-                            {
-                                playerCardRank[i] = "10";
-                                break;
-                            }
-                        case 11:
-                            {
-                                playerCardRank[i] = "J";
-                                break;
-                            }
-                        case 12:
-                            {
-                                playerCardRank[i] = "Q";
-                                break;
-                            }
-                        case 13:
-                            {
-                                playerCardRank[i] = "K";
-                                break;
-                            }
-                    }
-                }
-                if (dealerRank[i]==1)
-                {
-                    dealerCardRank[i] = "A";
-                    dealerCardVal = 11;
-                }
-                else if (dealerRank[i] < 10)
-                {
-                    dealerCardVal = dealerRank[i];
-                    dealerCardRank[i] = $"{dealerRank[i]}";
-                }
-                else if (dealerRank[i] >= 10)
-                {
-                    dealerCardVal = 10;
-                    switch (dealerRank[i])
-                    { 
-                        case 10:
-                        {
-                            dealerCardRank[i] = "10";
-                            break;
-                        }
-                        case 11:
-                            {
-                                dealerCardRank[i] = "J";
-                                break;
-                            }
-                        case 12:
-                            {
-                                dealerCardRank[i] = "Q";
-                                break;
-                            }
-                        case 13:
-                            {
-                                dealerCardRank[i] = "K";
-                                break;
-                            }
-                    }
-                }
-                playerSum += playerCardVal;
-                dealerSum += dealerCardVal;
-            }
-            
+            userHand = new List<Card>();
+            userHand.Add(deck.DrawACard()); //Annab mängijale esimesed
+            userHand.Add(deck.DrawACard()); //2 kaarti
 
-            Console.WriteLine($"You have been dealt: {playerCardRank[0]}{Suit[rnd.Next(0,4)]},{playerCardRank[1]}{Suit[rnd.Next(0, 4)]}");
-            Console.WriteLine($"House has been dealt: {dealerCardRank[0]}{Suit[rnd.Next(0, 4)]},[?]");
-            while(true)
+            foreach (Card card in userHand) //kui mängija kaartide hulgas on äss, siis saab väärtuseks 11
             {
-                if (playerSum == 21)
+                if (card.Face == Face.Ace)
                 {
-                    Console.WriteLine($"You have {playerSum} points vs. house {dealerSum} points");
-                    Console.WriteLine("You win!");
+                    card.Value = 11;
                     break;
                 }
+            }
+            Console.WriteLine($"You have been dealt: {userHand[0].Face} {userHand[0].Suit}, {userHand[1].Face} {userHand[1].Suit}");
+
+            dealerHand = new List<Card>();
+            dealerHand.Add(deck.DrawACard()); //Annab diilerile esimesed
+            dealerHand.Add(deck.DrawACard()); //2 kaarti
+
+            foreach (Card card in dealerHand)
+            {
+                if (card.Face == Face.Ace)
+                {
+                    card.Value = 11;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"House has been dealt: {dealerHand[0].Face} {dealerHand[0].Suit}, [?]\n");
+
+
+
+            if (userHand[0].Value + userHand[1].Value == 21) //kui mängija kaartide väärtus on esimese 2 kaardiga 21, siis ta on võitnud
+            {
+                Console.WriteLine("Blackjack, You Win!\n");
+                return;
+            }
+
+            do
+            {
                 Console.WriteLine("What do you want to do?");
                 Console.WriteLine("Choose: 1 - To take another card");
-                Console.WriteLine("Choose: 2 - To finish");
+                Console.WriteLine("Choose: 2 - To finish\n");
                 Console.Write("I choose: ");
-                string answer = Console.ReadLine();
-                if(answer == "1")
+                int userChoice = int.Parse(Console.ReadLine());
+                while (userChoice != 2 && userChoice != 1) //kui kasutaja sisestab vale sisendi, siis hoiatab sellest ja küsib uuesti sisendit
                 {
-                    playerRank[index] = rnd.Next(1, 14);
-                    dealerRank[index] = rnd.Next(1, 14);
-                    if (playerRank[index] == 1)
-                    {
-                        playerCardRank[index] = "A";
-                        playerCardVal = 11;
-                    }
-                    else if (playerRank[index] < 10)
-                    {
-                        playerCardVal = playerRank[index];
-                        playerCardRank[index] = $"{playerRank[index]}";
-                    }
-                    else if (playerRank[index] >= 10)
-                    {
-                        playerCardVal = 10;
-                        switch (playerRank[index])
-                        {
-                            case 10:
-                                {
-                                    playerCardRank[index] = "10";
-                                    break;
-                                }
-                            case 11:
-                                {
-                                    playerCardRank[index] = "J";
-                                    break;
-                                }
-                            case 12:
-                                {
-                                    playerCardRank[index] = "Q";
-                                    break;
-                                }
-                            case 13:
-                                {
-                                    playerCardRank[index] = "K";
-                                    break;
-                                }
-                        }
-                    }
-                    if (dealerRank[index] == 1)
-                    {
-                        dealerCardVal = 11;
-                    }
-                    else if (dealerRank[index] < 10)
-                    {
-                        dealerCardVal = dealerRank[index];
-                    }
-                    else if (dealerRank[index] >= 10)
-                    {
-                        dealerCardVal = 10;
-                    }
-                    playerSum += playerCardVal;
-                    dealerSum += dealerCardVal;
-                    Console.WriteLine($"You have been dealt: {playerCardRank[index]}{Suit[rnd.Next(0,4)]}");
-                    Console.WriteLine("House has been dealt: [?]");
-                    Console.WriteLine();
-                    if (playerSum == 21)
-                    {
-                    Console.WriteLine($"You have {playerSum} points vs. house {dealerSum} points");
-                    Console.WriteLine("You win!");
-                    break;
-                    }
-                    else if(playerSum>21)
-                    {
-                        Console.WriteLine($"You have {playerSum} points vs. house {dealerSum} points");
-                        Console.WriteLine("House wins!");
-                        break;
-                    }
+                    Console.WriteLine("Illegal key. Please choose a valid option: [1 - Hit | 2 - Stand]");
+                    userChoice = char.Parse(Console.ReadLine());
+                }
+                Console.WriteLine();
 
-                }
-                else if(answer == "2")
+                switch (userChoice)
                 {
-                    Console.WriteLine($"You have {playerSum} points vs. house {dealerSum} points");
-                    if(dealerSum > playerSum && dealerSum <= 21 || playerSum > 21)
-                    {
-                        Console.WriteLine("House wins!");
+                    case 1: //kasutaja tahab jätkata ja talle ning diilerile jagatakse veel 1 kaart
+                        userHand.Add(deck.DrawACard());
+                        Console.WriteLine($"\nYou have been dealt: {userHand[userHand.Count - 1].Face} {userHand[userHand.Count - 1].Suit}");
+                        int totalCardsValue = 0;
+                        foreach (Card card in userHand)
+                        {
+                            totalCardsValue += card.Value;
+                        }
+                        dealerHand.Add(deck.DrawACard());
+                        Console.WriteLine($"Dealer has been dealt: [?]\n");
+                        if (totalCardsValue > 21)
+                        {
+                            Console.Write("You have busted!\n");
+                            return;
+                        }
+                        else if (totalCardsValue == 21)
+                        {
+                            Console.WriteLine("You hit 21!");
+                            continue;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
+                    case 2:
+                        int playerCardsValue = 0;
+                        int dealerCardsValue = 0;
+                        foreach (Card card in userHand) //arvutab kokku mängija kaartide väärtuse
+                        {
+                            playerCardsValue += card.Value;
+                        }
+                        foreach (Card card in dealerHand) //arvutab kokku diileri kaartide väärtuse
+                        {
+                            dealerCardsValue += card.Value;
+                        }
+                        Console.WriteLine($"\nYou have {playerCardsValue} points vs. house {dealerCardsValue} points.");
+                        if(dealerCardsValue > 21)
+                            Console.WriteLine("You win!");
+                        else if(playerCardsValue > dealerCardsValue)
+                            Console.WriteLine("You win!");
+                        else if(playerCardsValue < dealerCardsValue)
+                            Console.WriteLine("House wins!");
+                        else if(playerCardsValue == dealerCardsValue)
+                            Console.WriteLine("Tie!");
+                        return;
+
+                    default:
                         break;
-                    }
-                    else if(playerSum > dealerSum && playerSum <= 21 || dealerSum > 21)
-                    {
-                        Console.WriteLine("You win!");
-                        break;
-                    }
-                    else if(playerSum == dealerSum)
-                    {
-                        Console.WriteLine("Tie!");
-                        break;
-                    }
                 }
-                else
-                {
-                    Console.WriteLine("Invalid input");
-                    continue;
-                }
-                index++;
+
+                Console.ReadLine();
             }
-            Console.ReadKey();
+            while (true);
         }
     }
 }
